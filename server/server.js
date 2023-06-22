a1c7ee0b13400331caa59fee507562e048d00367
Alex Durrant <alexjdurrant@gmail.com>
2023-06-22 13:05:45 -0600

    initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static('public'))
+
+
+
+
+
+app.listen(4000, console.log(`Server running on 4000!`))
