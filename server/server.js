8e8de21c42363353e3eeddbffac637a951f807c3
Alex Durrant <alexjdurrant@gmail.com>
2023-06-22 13:47:11 -0600

    fixing port

--- server/server.js
+++ server/server.js
@@ -6,10 +6,11 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use(express.static('public'))
+app.use(express.static(`public`))
 
-
-
+app.get('/', (req,res) => {
+    res.status(200).sendFile(path.join(__dirname,'../public/landingPage.html'))
+})
 
 
 app.listen(4000, console.log(`Server running on 4000!`))
